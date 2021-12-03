be6bac36d05406017d458cb14b72b64f4c3ed563
BaseerAzhar <32225656+BaseerAzhar@users.noreply.github.com>
2021-12-03 17:03:12 +0500

    update

--- test_hey.py
+++ test_hey.py
@@ -21,6 +21,27 @@ def test_send():
     pass  # perform some webtest test for your app
 
 
+@allure.feature("Fail")
+@allure.title('Fail')
+@allure.description('Fail')
+@allure.suite("Fail")
+@pytest.mark.check
+def test_send():
+    print("------------------------------------------------------------------------------------------")
+                
+    fail  # perform some webtest test for your app
+
+@allure.mark.skip
+@allure.feature("skip")
+@allure.title('skip')
+@allure.description('skip')
+@allure.suite("skip")
+@pytest.mark.check
+def test_send():
+    print("------------------------------------------------------------------------------------------")
+                
+    pass  # perform some webtest test for your app
+
 
 
 def test_something_quick():
